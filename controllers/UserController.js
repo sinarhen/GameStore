992ee878a4d16ade0eb9ed27a58b4ddedba757e5
RostyslavSQX <rostikboss2005@gmail.com>
2024-01-14 15:33:10 +0200

    get me

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -191,3 +191,25 @@ export const deleteUser = async (req, res) => {
         });
     }
 }
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(userData);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get user',
+        })
+    }
+}
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,13 @@
-import { register, login, changeEmail, changePassword, deleteUser } from "../controllers/UserController.js";
+import { register, login, changeEmail, changePassword, deleteUser, getMe } from "../controllers/UserController.js";
 import express from 'express';
+import checkAuth from "../utils/checkAuth.js";
 
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
-router.put("/change-email",/* TODO: checkAuth, */ changeEmail);
+router.put("/change-email", checkAuth, changeEmail);
 router.put("/change-password",/* TODO: checkAuth, */ changePassword);
 router.delete("/delete",/* TODO: checkAuth, */ deleteUser);
+router.get("/me", checkAuth, getMe);
+
 export default router;
