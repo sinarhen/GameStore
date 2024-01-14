088f4c037272efb09bafa27707975be01c151e2e
RostyslavSQX <rostikboss2005@gmail.com>
2024-01-14 15:48:00 +0200

    order model

---
+++ models/Order.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    }, 
+    {
+    timestamps: true
+    },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
