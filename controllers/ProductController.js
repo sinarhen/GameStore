4d4c57f0b522f72abc3a99308fb87197ac5b15a5
sinarhen <nicklchannel8@gmail.com>
2024-01-14 12:53:02 +0200

    Using util function to create JWT

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,14 +1,48 @@
 import Product from './Product'; // Replace with the actual path to your Product model
+import { check, validationResult } from 'express-validator';
 
 class ProductController {
     // Get all products
+
+    static validate(method) {
+        switch (method) {
+            case 'createProduct': {
+                return [
+                    check('name', 'Name is required').notEmpty(),
+                    check('description', 'Description is required').notEmpty(),
+                    check('price', 'Price is required').isNumeric(),
+                    check('imageUrl', 'Image URL is required').isURL(),
+                    check('categoryId', 'Category ID is required').notEmpty(),
+                ];
+            }
+            case 'updateProduct': {
+                return [
+                    check('id', 'ID is required').notEmpty(),
+                    check('name', 'Name is required').notEmpty(),
+                    check('description', 'Description is required').notEmpty(),
+                    check('price', 'Price is required').isNumeric(),
+                    check('imageUrl', 'Image URL is required').isURL(),
+                    check('categoryId', 'Category ID is required').notEmpty(),
+                ];
+            }
+            case 'deleteProduct': {
+                return [
+                    check('id', 'ID is required').notEmpty(),
+                ];
+            }
+        }
+    }
+
     async getAllProducts(req, res) {
-        const { categoryId, orderBy, filterBy } = req.query;
-        
+        const { categoryId, orderBy, filterBy, searchTerm } = req.query;
         let query = Product.find();
 
         if (categoryId) {
             query = query.where('categoryId').equals(categoryId);
+        }
+
+        if (searchTerm) {
+            query = query.where('name').regex(new RegExp(searchTerm, 'i'));
         }
 
         if (orderBy) {
@@ -30,7 +64,7 @@ class ProductController {
     // Get product by id
     async getProductById(req, res) {
         const { id } = req.params;
-
+        
         try {
             const product = await Product.findById(id);
             if (product) {
@@ -42,6 +76,74 @@ class ProductController {
             res.status(500).send(err);
         }
     }
+
+    async createProduct(req, res) {
+        const { name, description, price, imageUrl, categoryId } = req.body;
+
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            
+            // TODO: VALIDATE ADMIN NAME
+            const product = await Product.create({
+                name,
+                description,
+                price,
+                imageUrl,
+                categoryId,
+            });
+
+            res.json(product);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+    
+    async updateProduct(req, res) {
+        const { id } = req.params;
+        const { name, description, price, imageUrl, categoryId } = req.body;
+
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            // TODO: VALIDATE ADMIN NAME
+            const product = await Product.findByIdAndUpdate(id, {
+                name,
+                description,
+                price,
+                imageUrl,
+                categoryId,
+            });
+
+            res.json(product);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
+    async deleteProduct(req, res) {
+        const { id } = req.params;
+        
+            // TODO: VALIDATE ADMIN NAME
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const product = await Product.findByIdAndDelete(id);
+            res.json(product);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
+    
 }
 
 export default ProductController;
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,8 @@
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import UserModel from '../models/User.js';
 import { generateToken } from '../utils/jwt.js';
+
+
 
 export const register = async (req, res) => {
     try {
