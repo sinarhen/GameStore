16d15a0e7d97336e98b88c77cae5492795998b31
sinarhen <nicklchannel8@gmail.com>
2024-01-15 13:25:25 +0200

    Frontend recreated with typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Categories from "./pages/Categories";
+import MyAccount from "./pages/MyAccount";
+import Favorites from "./pages/Categories";
+import Orders from "./pages/Orders";
+
+function App() {
+  return (
+    <div className="h-full w-full text-white bg-neutral-900">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/myaccount' element={<MyAccount />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='/orders' element={<Orders />} />
+        </Routes>
+      </Router>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Categories.tsx
@@ -0,0 +1,8 @@
+export default function Favorites()
+{
+    return(
+        <div className='favorites'>
+            <h1>Favorites</h1>
+        </div>
+    )
+}
