e68d447971b0d2c41fef49ec3f1e0e850f0c79c5
sinarhen <nicklchannel8@gmail.com>
2024-01-21 14:38:29 +0200

    MyFavorites.tsx

---
+++ client/src/components/MyFavorites.tsx
@@ -0,0 +1,38 @@
+import { motion } from "framer-motion";
+import { ProductCardType } from "../lib/types";
+import ProductCard from "./ProductCard";
+import NotFound from "./NotFound";
+
+export default function MyFavorites({favorites}: {
+    favorites: ProductCardType[]
+}){
+    if (!favorites.length) return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ delay: 3, duration: 1}}
+            className="mt-4"
+        >
+            <NotFound helperText="Try to add something to favorites first."/>
+        </motion.div>
+    )
+    return (
+        <div className="flex gap-x-4 w-full mt-10 overflow-x-scroll snap snap-x" style={{scrollSnapType: 'x mandatory', scrollBehavior: 'smooth'}}>
+            {favorites.map((favorite: any, index: number) => (
+                <motion.div
+                    key={favorite._id + index}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ delay: 0.7 + index * 0.1, duration: 1}}
+                    className="snap-start w-[95%] sm:w-1/2  md:w-1/3 lg:w-1/4"
+                    style={{scrollSnapAlign: 'start', flex: '0 0 auto'}}
+                >
+                    <ProductCard key={favorite._id} product={favorite}/>
+                </motion.div>
+            ))}
+        </div>
+    )                       
+                  
+}
--- client/src/pages/MyAccount.tsx
+++ client/src/pages/MyAccount.tsx
@@ -9,6 +9,7 @@ import ProductCard from "../components/ProductCard";
 import { motion } from "framer-motion";
 import NotFound from '../components/NotFound';
 import Section from "../components/Section";
+import MyFavorites from "../components/MyFavorites";
 
 export default function MyAccount() {
     const { user } = useCurrentUser();
@@ -41,36 +42,7 @@ export default function MyAccount() {
         <Section className="h-full">
                 <Header animateableText="Favorites" appearDuration={0.2} />
                 <AnimatedSeparator appearDuration={0.3}/>
-                {favorites.length ? (
-                    <div className="flex gap-x-4 w-full mt-10 overflow-x-scroll snap snap-x" style={{scrollSnapType: 'x mandatory', scrollBehavior: 'smooth'}}>
-                    {favorites.map((favorite: any, index: number) => (
-                        <motion.div
-                            key={favorite._id + index}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ delay: 0.7 + index * 0.1, duration: 1}}
-                            className="snap-start w-[95%] sm:w-1/2  md:w-1/3 lg:w-1/4"
-                            style={{scrollSnapAlign: 'start', flex: '0 0 auto'}}
-                        >
-                            <ProductCard key={favorite._id} product={favorite}/>
-                        </motion.div>
-                    ))}
-                </div>
-                        
-                ): (
-                 <>
-                        <motion.div
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ delay: 3, duration: 1}}
-                            className="mt-4"
-                        >
-                            <NotFound helperText="Try to add something to favorites first."/>
-                        </motion.div>
-                 </>   
-                )}
+                <MyFavorites favorites={favorites}/>
         
         </Section>
             </>
