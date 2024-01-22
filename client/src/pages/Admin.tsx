87183cb9149fd2f63707f83b3ac25ee97305cc11
Rostyslav <rostikboss2005@gmail.com>
2024-01-22 12:58:13 +0200

    admin panel

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,6 +1,24 @@
-export default function Admin() {
-    return (
-        <h1>Change Order Status</h1>
+import Input from "../components/Input";
+import Button from "../components/Button";
 
+import React from "react";
+import { getOrder } from "../lib/order";
+
+export default function Admin() {
+    const [id, setId] = React.useState("");
+
+
+    return (
+        <>
+            <h1 className="pb-4" >Change Order Status</h1>
+            <Input
+                name="orderId"
+                type="text"
+                placeholder="Order ID"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+            />
+            <Button className="mt-4" onClick={() => getOrder(id)}>Search</Button>
+        </>
     );
 }
