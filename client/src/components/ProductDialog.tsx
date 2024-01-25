c7099ee34a2c61db0b8c895ee8350ed8db635326
sinarhen <nicklchannel8@gmail.com>
2024-01-25 02:15:27 +0200

    Fixed product dialog styling

--- client/src/components/ProductDialog.tsx
+++ client/src/components/ProductDialog.tsx
@@ -3,6 +3,9 @@ import { ProductCardType } from '../lib/types';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./Dialog"
 import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "./Table"
 import EditProductDialog from "./EditProductDialog";
+import Clipboard from "./Clipboard";
+import { formatter } from "../lib/utils";
+import EditProfileDialog from './EditProfileDialog';
 
 
 interface ProductDialogProps {
@@ -22,33 +25,38 @@ const ProductDialog: React.FC<ProductDialogProps> = ({
             <Dialog open={open} onOpenChange={() => setOpen(false)}>
             <DialogContent className="min-w-[95%] md:min-w-[75%]">
                 <DialogHeader>
-                <DialogTitle>Product {product?.name} </DialogTitle>
-                <DialogDescription>
-                    <img src={product?.imageUrl} className="w-full h-full max-h-96 max-w-96" />
-                </DialogDescription>
+                    <DialogTitle className="">Product <span className="animate-pulse text-indigo-600">{product?.name}</span> information</DialogTitle>
                 </DialogHeader>
-                <Table>
-                <TableHeader>
-                    <TableRow>
-                    <TableHead>Name</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead className="text-center">Action</TableHead>
+                <div className="sm:flex-row flex flex-col overflow-y-scroll  gap-x-2 ">
+                            <img alt="No photo" className="rounded-lg w-full sm:w-1/2 object-cover object-center" 
+                                src={product?.imageUrl}>
+                            </img>
+                        <div className="flex flex-col justify-between ">
+                            <div>
+                                <h1 className="text-3xl text-indigo-600">{product?.name}</h1>
+                                {product?._id && (
+                                    <p className="text-gray-500 text-sm">{product._id} <Clipboard text={product?._id} /></p>
+                                )}
+                                <p className="text-muted-foreground mt-2 text-sm overflow-y-auto sm:max-h-[150px]">
+                                    {product?.description}
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa vero modi architecto reprehenderit quaerat magnam autem, voluptatum illo minima repellendus asperiores enim quidem accusamus natus consectetur corrupti ut consequatur. Perferendis.
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa vero modi architecto reprehenderit quaerat magnam autem, voluptatum illo minima repellendus asperiores enim quidem accusamus natus consectetur corrupti ut consequatur. Perferendis.
+                                </p>    
+                            </div>
+                            
+                            <div className="flex justify-between items-center mt-2">
+                                <p className="text-indigo-600 text-lg">{formatter.format(product?.price!)}</p>
+                                <EditProductDialog initialValues={{
+                                    name: product?.name,
+                                    price: product?.price,
+                                    description: product?.description,
+                                    imageUrl: product?.imageUrl,
+                                }}/>
+                            </div>
+                            
+                        </div>
 
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableCell>{product?.name}</TableCell>
-                    <TableCell>{product?.price}</TableCell>
-                    <TableCell className="text-center">
-                        <EditProductDialog initialValues={{
-                            name: product?.name,
-                            price: product?.price,
-                            description: product?.description,
-                            imageUrl: product?.imageUrl
-                        }}/>
-                    </TableCell>
-                </TableBody>
-                </Table>
+                    </div>
             </DialogContent>
             </Dialog>
             
