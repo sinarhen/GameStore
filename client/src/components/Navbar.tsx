0789fe8f45c5a3f9154fa8bcc4ca0298535f05e3
sinarhen <nicklchannel8@gmail.com>
2024-01-29 19:17:50 +0200

    Added logo

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,7 +14,10 @@ const Navbar = () => {
       <nav className="bg-neutral-800 z-50 fixed w-full">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex duration-200 items-center justify-between h-16">
-            <h1 className="text-white"><Link to="/">Mk-Shop</Link></h1>
+            <Link className='flex gap-x-2 items-center' to="/">
+              <img className={"h-12 w-auto"} src="/logo.png" alt={"not found"} />
+              <h1 className="text-white">Mk-Shop</h1>
+            </Link>
             <div className="flex">
               <NavLink to="/"
                        className={({isActive}) => `hover:bg-neutral-700 transition-colors px-3 py-2 rounded-md ${!isActive ? "text-gray-300" : "text-indigo-400"}`}>Home</NavLink>
